9194dc63aded9cd8ce2b70f419d61862f5d363e3
addthoriq <alvarro.thoriq@gmail.com>
2021-08-22 21:03:28 +0800

    first commit

---
+++ 01hello.kts
@@ -0,0 +1,9 @@
+// Run biasa
+// println("Hello world");
+
+// Pake function
+fun main(){
+	println("Hello Bosque");
+}
+
+main();
---
+++ 02variable.kts
@@ -0,0 +1,19 @@
+val umur = 21;
+val nama = "Thoriq";
+// println(umur);
+// println(nama);
+
+// var itu bisa diubah nilai nya
+var alamat = "Samarinda";
+alamat = "Balikpapan";
+// println(alamat);
+
+// Ini error, karena val tidak dapat di reassigned
+// nama = "Haninoy";
+// println(nama);
+
+// Penulisan Variabel yang benar:
+var namaDepan = "Muhammad";
+var namaDepan2 = "Abu";
+var nama_depan = "Rizqy";
+// Selain diatas, salah
